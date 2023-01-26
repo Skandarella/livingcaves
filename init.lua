e732340c1d1fe98a4ec60ba7561c6c9acc0d0fc3
Skandarella <75903112+Skandarella@users.noreply.github.com>
2023-01-27 00:08:59 +0100

    Add files via upload

--- init.lua
+++ init.lua
@@ -18,17 +18,7 @@ if livingcaves.settings.clear_ores then
 	minetest.clear_registered_ores()
 end
 
-local modname = "livingcaves"
-local modpath = minetest.get_modpath(modname)
-local mg_name = minetest.get_mapgen_setting("mg_name")
-
--- Load support for intllib.
 local path = minetest.get_modpath(minetest.get_current_modname()) .. "/"
-
-local S = minetest.get_translator and minetest.get_translator("livingcaves") or
-		dofile(path .. "intllib.lua")
-
-
 
 -- plants
 dofile(path .. "cavenodes.lua") -- 
@@ -40,4 +30,3 @@ dofile(path .. "hunger.lua") --
 
 
 
-print (S("[MOD] Caves loaded"))
